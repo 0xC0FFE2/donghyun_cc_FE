5ced0b5b0f64d6e9827e4d28e4c34ee617b1ca22
hy2n <github1@ncloud.sbs>
2024-10-13 06:44:17 +0900

    chore : 방문자 표시 기능 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ function Profile() {
       <ProfileCard />
       <CountCard />
       <RecentPosts size='4' />
-      <CategoryPosts size='4' mode="AUTO" />
+      <CategoryPosts size='8' mode="AUTO" />
     </div>
   );
 }
